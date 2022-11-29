3eeedb7135ce22324510597c34fd2695de484cc1
Nguyên <nguyenndm1901@gmail.com>
2022-11-29 12:31:54 +0700

    Update AlbumRepository.java

--- src/main/java/com/java/chinook/interfaces/AlbumRepository.java
+++ src/main/java/com/java/chinook/interfaces/AlbumRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
-    Optional<Album> findByAlbumId(int Id);
+    Optional<Album> findByAlbumId(int albumId);
 
     List<Album> findAllByOrderByAlbumIdAsc();
 }
